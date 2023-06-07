d461638949f2117169ccd9fac24eedc0f047ff38
tessi <tessi.what.why.how@gmail.com>
2023-06-07 21:27:15 +0200

    player winning

--- Assets/Scripts/Checkpoint/PlayerCheckpoint.cs
+++ Assets/Scripts/Checkpoint/PlayerCheckpoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerCheckpoint : MonoBehaviour
@@ -9,16 +10,46 @@ public class PlayerCheckpoint : MonoBehaviour
     public int currentLap = 0;
     public int maxLap = 3;
 
+    PlayerController playerController;
+    public TextMeshProUGUI winnigText;
+    public TextMeshProUGUI lappText;
+
+    private void Start()
+    {
+        playerController = gameObject.GetComponentInChildren<PlayerController>();
+        lappText.text = "Lap: " + currentLap + " / " + maxLap;
+    }
+
 
     private void Update() {
+
         if (currentLap >= maxLap) {
             winCondition();
         }
+        lappText.text = "Lap: " + currentLap + " / " + maxLap;
+
 
     }
 
     private void winCondition() {
         Debug.Log("You win");
+
+        if (playerController.secondPlayer == false)
+        {
+            GameObject pc = GameObject.Find("PlayerCanvas");
+            TextMeshProUGUI wi = Instantiate(winnigText, pc.transform);
+            wi.gameObject.SetActive(true);
+
+        }
+        else
+        {
+            GameObject pc = GameObject.Find("PlayerCanvas Variant");
+            TextMeshProUGUI wi = Instantiate(winnigText, pc.transform);
+            wi.gameObject.SetActive(true);
+           
+        }
+
+
     }
 
 }
